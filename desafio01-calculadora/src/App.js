a8607c899c298a6190ec997fd71c4936ee03db8d
Nuno Rocha <nunorulz@gmail.com>
2026-07-15 17:15:16 -0300

    Adiciona pastas dio e desafio01-calculadora como conteúdo normal

---
+++ desafio01-calculadora/src/App.js
@@ -0,0 +1,15 @@
+import Input from "./components/Input"
+import Button from "./components/Button"
+import { Container, Content } from "./styles";
+
+const App = () => {
+  return (
+    <Container>
+      <Content>
+      <Input/>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ dio/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React!</h1>
+      <Button title="Entrar"/>
+    </div>
+  );
+}
+
+export default App;
